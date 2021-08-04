f122d4deece2ed3fe75d3b53a9ea4cf4bd500bf1
aoito <ao130324@gmail.com>
2021-08-04 17:18:32 +0900

    test

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class ="header">
+        <div class = "header-inner">
+            <h1 class ="title" @click="clickSmoothScroll('#app')">ao ito</h1>
+        </div>
+    </div>
+</template>
